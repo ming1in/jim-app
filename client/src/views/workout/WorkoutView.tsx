71d847f26e18cb403ff24ce63877ea5e5b3ca28b
Megha Mansuria <mmansuri@stevens.edu>
2021-05-11 18:35:21 -0400

    separate optionsform file stuff
    put the form in another .tsx file to make OptionsView simpler (and to follow set up of other files)

--- client/src/views/workout/WorkoutView.tsx
+++ client/src/views/workout/WorkoutView.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 
-import { Box, Typography, Button } from '@material-ui/core';
+import { Box, Typography, Button, Card, CardContent, Container, makeStyles, createStyles } from '@material-ui/core';
 import { getUsers } from '../../api/users';
+import { MenuItem } from 'material-ui';
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    container: {
+      display: "flex",
+      alignItems: "center",
+    },
+  })
+);
 
 function WorkoutView() {
+  const classes = useStyles();
   return (
-    <Box >
-      <Typography>Workout Modes</Typography>
-      <Button variant="contained" href="./BuildView" color="primary">Build A Custom Workout</Button>
-      <Button variant="contained" href="./OptionsView" color="secondary">Select A Pre-Made Workout</Button>
+    <Box display="flex" justifyContent="center" flexDirection="column">
+      <Container maxWidth="sm" className={classes.container}>
+        <Typography>Workout Modes</Typography>
+      </Container>
+      <Card>
+        <CardContent>
+          <Button variant="contained" href="./BuildView" color="primary">Build A Custom Workout</Button>
+          <Button variant="contained" href="./OptionsView" color="secondary">Select A Pre-Made Workout</Button>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
