38638bd9149dc7a3c1bec2cbc73b368a2cf810e1
Megha Mansuria <mmansuri@stevens.edu>
2021-05-13 17:06:40 -0400

    started building

--- client/src/views/workout/BuildView.tsx
+++ client/src/views/workout/BuildView.tsx
@@ -1,6 +1,20 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import { Box, Typography, Button, Container } from '@material-ui/core';
+import {
+  Box,
+  Typography,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  Card,
+  CardContent,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from '@material-ui/core';
 import { getUsers } from '../../api/users';
 import WorkoutView from './WorkoutView';
 import LaunchView from './LaunchView';
@@ -12,13 +26,39 @@ interface IBuildViewProps {
 }
 
 function BuildView(props: IBuildViewProps) {
+  const temp = ["crunches", "sit ups", "push ups"];
   return (
+
     <Box>
-      {/* <Container maxWidth="sm" >
-        <Button variant="contained" href="./workout" color="secondary">Go Back to Workout Choices</Button>
-      </Container> */}
-      <Typography>Build Your Own Workout</Typography>
-      <Button variant="contained" onClick={() => props.setStep('launch')}>Continue to Workout</Button>
+      <Grid container direction="column" alignItems="center">
+        <Grid item>
+          <Box display="flex" margin={5}>
+            <Card>
+              <Table>
+                <TableHead>
+                  <Typography variant="h3" color="textSecondary" align="center">Category List of Exercises</Typography>
+                </TableHead>
+                <TableBody>
+                  {temp.map(n => {
+                    return (
+                      <TableRow>
+                        <TableCell>
+                          <Typography variant="body2" color="textSecondary">{n}</Typography>
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </Card>
+          </Box>
+        </Grid>
+        <Grid item>
+          <Box display="flex" margin={5}>
+            <Button variant="contained" onClick={() => props.setStep('launch')}>Continue to Workout</Button>
+          </Box>
+        </Grid>
+      </Grid>
     </Box>
   );
 }
