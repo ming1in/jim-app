c615173318a82e11f37574c5dfec02f9f6d353b3
Constance  Xu <cxu16@stevens.edu>
2021-05-13 15:32:19 -0500

    profile view changes

--- client/src/views/profile/ProfileView.tsx
+++ client/src/views/profile/ProfileView.tsx
@@ -14,11 +14,18 @@ import {
     createStyles,
     Avatar,
     CardActions,
-    Grid
+    Grid,
+    Table,
+    TableCell,
+    TableRow,
+    TableBody,
+    TableHead,
+    Paper
 }
     from '@material-ui/core';
 import { getUsers } from '../../api/users';
 import EditProfileForm from "./EditProfileForm";
+import { createDeflateRaw } from 'node:zlib';
 
 const useStyles = makeStyles((theme) =>
     createStyles({
@@ -27,9 +34,8 @@ const useStyles = makeStyles((theme) =>
             color: "primary"
         },
         card: {
-            alignItems: "center",
-            color: "primary"
-        }
+            alignItems: "center"
+        },
     })
 );
 
@@ -47,59 +53,97 @@ function ProfileView() {
         height: "200",
         age: 25,
         gender: "Male",
-        goals: "To run a mile!"
+        goals: "To run a mile!",
+        workoutIds: ["workout1", "workout2", "workout3", "workout4", "workout5"]
     }
     let pronouns = "he/him"; //default pronouns
     if (temp.gender == "Male") {
         pronouns = "he/him";
     } else if (temp.gender == "Female") {
         pronouns = "she/her";
-    } else {
+    } else { //other
         pronouns = "they/them";
     }
-    let randomQuote = ["You can do it!", "The only person stopping you, is you", "Push yourself", "Just do it", "Shoot for the moon, even if you miss, you'll land among the stars"];
+    let randomQuote = ["You can do it!", "The only person stopping you, is you", "Do something today that your future self will thank you for", "Just do it", "Shoot for the moon, even if you miss, you'll land among the stars", "The pain you feel today, will be the strength you feel tomorrow", "The only bad workout is the one that did not happen", "When you feel like quitting, think about why you started"];
     let chosenRandomQuote = randomQuote[Math.floor(Math.random() * randomQuote.length)];
 
     return (
-        <Container className={classes.container}>
-            <Card>
-                <Grid container direction="column" alignItems="center" spacing={3}>
-                    <Grid item spacing={3}>
-                        <Typography gutterBottom variant="h1" component="h2" color="textPrimary">
-                            👋 Hello, {temp.firstName} {temp.lastName}
-                        </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h4" component="p" color="secondary">
-                            ✨ {chosenRandomQuote} ✨
+        <Box>
+            <Box margin={5}>
+                <Typography gutterBottom variant="h1" component="h2" color="textPrimary" align="center">
+                    👋 Hello, {temp.firstName} {temp.lastName}
+                </Typography>
+            </Box>
+            <Grid container direction="column" alignItems="center">
+                <Grid item>
+                    <Box display="flex" margin={5}>
+                        <Paper>
+                            <Typography variant="h4" component="p" color="textSecondary" align="center">
+                                ✨ {chosenRandomQuote} ✨
                     </Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h3" color="textSecondary" component="p">You live in {temp.city}</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h3" color="textSecondary" component="p">You weigh {temp.weight} kilograms</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h3" color="textSecondary" component="p">You are {temp.height} inches</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h3" color="textSecondary" component="p">You are {temp.age} years old</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h3" color="textSecondary" component="p">Your pronouns are {pronouns}</Typography>
-                    </Grid>
-                    <Grid item>
-                        <Typography variant="h3" color="textSecondary" component="p">Your goal is {temp.goals.toLowerCase()}</Typography>
-                    </Grid>
-                    <Grid item>
+                        </Paper>
+                    </Box>
+                </Grid>
+                <Grid item>
+                    <Box display="flex" margin={5}>
+                        <Card>
+                            <CardContent style={{ backgroundColor: "secondary" }}>
+                                <Grid container direction="column" spacing={3} alignItems="center">
+                                    <Grid item>
+                                        <Typography variant="h3" color="textSecondary" component="p">You live in <Box fontWeight='bold' display='inline'>{temp.city}</Box></Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h3" color="textSecondary" component="p">You weigh <Box fontWeight='bold' display='inline'>{temp.weight}</Box> kilograms</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h3" color="textSecondary" component="p">You are <Box fontWeight='bold' display='inline'>{temp.height}</Box> inches</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h3" color="textSecondary" component="p">You are <Box fontWeight='bold' display='inline'>{temp.age}</Box> years old</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h3" color="textSecondary" component="p">Your pronouns are <Box fontWeight='bold' display='inline'>{pronouns}</Box></Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant="h3" color="textSecondary" component="p">Your goal is <Box fontWeight='bold' display='inline'>{temp.goals.toLowerCase()}</Box></Typography>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Box>
+                </Grid>
+                <Grid item>
+                    <Box display="flex" margin={5}>
+                        <Card>
+                            <Table>
+                                <TableHead>
+                                    <Typography variant="h3" color="textSecondary" align="center">Workouts Saved</Typography>
+                                </TableHead>
+                                <TableBody>
+                                    {temp.workoutIds.map(n => {
+                                        return (
+                                            <TableRow>
+                                                <TableCell>
+                                                    <Typography variant="body2" color="textSecondary">{n}</Typography>
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                                </TableBody>
+                            </Table>
+                        </Card>
+                    </Box>
+                </Grid>
+                <Grid item>
+                    <Box display="flex" margin={5}>
                         <Button size="large" variant="contained" color="secondary">
                             Edit Profile
-                    </Button>
-                    </Grid>
+            </Button>
+                    </Box>
                 </Grid>
-            </Card>
-        </Container>
+            </Grid>
+        </Box>
+
     );
 }
 
